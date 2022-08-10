78c89bf502f2af8e636f535a6ea89eb80ee9e2aa
Loiroo <henriquemc40@gmail.com>
2022-08-10 14:57:28 -0300

    commit home e style projeto site php

---
+++ home.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="">
+  <head>
+    <meta charset="utf-8">
+    <title>Hora de Trollar</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+
+
+
+<div class="div1">
+          <p class="texto1">Make your choice!</p>
+
+    <img src="imagens/top_icon.png" class="topicon">
+    <img src="imagens/jg_icon.png" class="jgicon">
+    <img src="imagens/mid_icon.png" class="midicon">
+    <img src="imagens/adc_icon.png" class="adcicon">
+    <img src="imagens/sup_icon.png" class="supicon">
+
+      <a href="lane.php"><button class="btntop" type="button">TOP</button></a>
+      <a href="jg.php"><button class="btnjg" type="button">JG</button></a>
+      <a href="lane.php"><button class="btnmid" type="button">MID</button></a>
+      <a href="lane.php"><button class="btnadc" type="button">ADC</button></a>
+      <a href="sup.php"><button class="btnsup" type="button">SUP</button></a>
+
+</div>
+
+
+
+
+  </body>
+</html>
